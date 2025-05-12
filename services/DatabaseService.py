531ecb60f71f1d74c1c1da21f0b23a2abe5bd759
Brian Salchert <brisalchert@gmail.com>
2025-05-12 16:05:41 -0400

    Implement ModelEditor in DatabaseService

--- services/DatabaseService.py
+++ services/DatabaseService.py
@@ -1,11 +1,13 @@
 import io
 import psycopg
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from model import DataModel
 from services.AbstractService import AbstractService
 from services.DatabaseAccess import DatabaseAccess
+from services.ModelEditor import ModelEditor
 
-class DatabaseService(AbstractService, DatabaseAccess):
+
+class DatabaseService(AbstractService, DatabaseAccess, ModelEditor):
     @property
     def db_connection(self) -> psycopg.Connection:
         return self._db_connection
@@ -23,6 +25,8 @@ class DatabaseService(AbstractService, DatabaseAccess):
         self._db_connection = None
         self._data_file = None
 
+    # --- DatabaseAccess overrides ---
+
     def set_connection(self, database: str, user: str, host: str, password: str, port: int = 5432):
         self._db_connection = psycopg.connect(database=database, user=user, host=host, password=password, port=port)
 
@@ -38,6 +42,22 @@ class DatabaseService(AbstractService, DatabaseAccess):
     def get_tables(self) -> dict[str, DataFrame]:
         # TODO: Implement get_tables
         pass
+
+    # --- ModelEditor overrides ---
+
+    def create_row(self, table_name: str, columns: dict) -> bool:
+        return self._model.create_row(table_name, columns)
+
+    def read_row(self, table_name: str, primary_key: str) -> Series:
+        return self._model.read_row(table_name, primary_key)
+
+    def update_row(self, table_name: str, primary_key: str, columns: dict) -> bool:
+        return self._model.update_row(table_name, primary_key, columns)
+
+    def delete_row(self, table_name: str, primary_key: str) -> Series:
+        return self._model.delete_row(table_name, primary_key)
+
+    # --- Subclass methods ---
 
     def load_from_database(self) -> bool:
         # TODO: Implement load_from_database
