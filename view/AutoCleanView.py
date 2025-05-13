11f4cd0036703a632c2acf471197fb27ffda4474
Brian Salchert <brisalchert@gmail.com>
2025-05-13 14:44:34 -0400

    Create AutoCleanView

---
+++ view/AutoCleanView.py
@@ -0,0 +1,51 @@
+from PyQt6.QtWidgets import QWidget
+from navigation import NavigationController
+from view.AbstractView import AbstractView
+from viewmodel import AutoCleanViewModel
+
+class AutoCleanView(QWidget, AbstractView):
+    @property
+    def view_model(self):
+        return self._view_model
+
+    @property
+    def nav_controller(self):
+        return self._nav_controller
+
+    def __init__(self, view_model: AutoCleanViewModel, nav_controller: NavigationController):
+        super().__init__()
+        self._view_model = view_model
+        self._nav_controller = nav_controller
+        self.cleaning_config: dict = {}
+        self.analytics_config: dict = {}
+        self.cleaning_running: bool = False
+        self.progress: float = 0
+        self.current_step: str = ""
+        self.cleaning_stats: dict = {}
+
+        # Connect ViewModel to UI
+        self._view_model.nav_destination_changed.connect(self.navigate)
+        self._view_model.cleaning_config_changed.connect(self.update_cleaning_config)
+        self._view_model.analytics_config_changed.connect(self.update_analytics_config)
+        self._view_model.cleaning_running_changed.connect(self.update_running)
+        self._view_model.progress_updated.connect(self.update_progress)
+        self._view_model.current_step_changed.connect(self.update_step)
+        self._view_model.cleaning_stats_updated.connect(self.update_stats)
+
+    def update_cleaning_config(self, cleaning_config: dict):
+        self.cleaning_config = cleaning_config
+
+    def update_analytics_config(self, analytics_config: dict):
+        self.analytics_config = analytics_config
+
+    def update_running(self, running: bool):
+        self.cleaning_running = running
+
+    def update_progress(self, progress: float):
+        self.progress = progress
+
+    def update_step(self, step: str):
+        self.current_step = step
+
+    def update_stats(self, stats: dict):
+        self.cleaning_stats = stats
