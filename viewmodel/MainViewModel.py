7f451f1572da501a94ea8fe0257e5d377a51892b
Brian Salchert <brisalchert@gmail.com>
2025-05-13 14:09:00 -0400

    Fix pyqtSignal structure in view model

--- viewmodel/MainViewModel.py
+++ viewmodel/MainViewModel.py
@@ -1,38 +1,42 @@
-from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtCore import pyqtSignal, QObject
 from pandas import DataFrame
 from navigation import Screen
 from services import DatabaseService, DataEditorService
 from viewmodel import ViewModel
 
-
-class MainViewModel(ViewModel):
-    @property
-    def nav_destination(self) -> Screen:
-        return self._nav_destination
-
-    @property
-    def data_changed(self) -> pyqtSignal(list[DataFrame]):
-        return self._data_changed
-
-    @property
-    def database_loaded(self) -> pyqtSignal(bool):
-        return self._database_loaded
+class MainViewModel(QObject, ViewModel):
+    # --- Signals for view ---
+    nav_destination_changed = pyqtSignal(Screen)
+    data_changed = pyqtSignal(dict[str, DataFrame])
+    database_loaded_changed = pyqtSignal(bool)
 
     def __init__(self, database_service: DatabaseService, data_editor_service: DataEditorService):
+        super().__init__()
         self.database_service = database_service
         self.data_editor_service = data_editor_service
         self._nav_destination = Screen.MAIN
-        self._data_changed = None
-        self._database_loaded = False
+        self._data = None
+        self._database_loaded = None
 
     def set_nav_destination(self, destination: Screen):
         self._nav_destination = destination
+        self.nav_destination_changed.emit(destination)
 
     def load_database(self, database: str, user: str, host: str, password: str, port: int = 5432):
         self.database_service.set_connection(database, user, host, password, port)
+        self.database_service.load_from_database()
+        self._data = self.database_service.get_tables()
+        self._database_loaded = True
+        self.data_changed.emit(self._data)
+        self.database_loaded_changed.emit(self._database_loaded)
 
     def load_file(self, file_path: str):
         self.database_service.set_file(file_path)
+        self.database_service.load_from_file()
+        self._data = self.database_service.get_tables()
+        self._database_loaded = True
+        self.data_changed.emit(self._data)
+        self.database_loaded_changed.emit(self._database_loaded)
 
     def reset_data(self):
         self.database_service.reset_data()
@@ -49,6 +53,3 @@ class MainViewModel(ViewModel):
 
     def export_data(self):
         self.data_editor_service.export_data()
-
-    def navigate_to(self, destination: Screen):
-        self.set_nav_destination(destination)
--- viewmodel/ViewModel.py
+++ viewmodel/ViewModel.py
@@ -2,12 +2,6 @@ from abc import ABC, abstractmethod
 from navigation import Screen
 
 class ViewModel(ABC):
-    @property
-    @abstractmethod
-    def nav_destination(self) -> Screen:
-        """Abstract property for navigation destination"""
-        pass
-
     @abstractmethod
     def set_nav_destination(self, destination: Screen):
         """Abstract setter for navigation destination"""
