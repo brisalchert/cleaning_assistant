34182faefb91c84393dbeb872680dd7063a689e8
Brian Salchert <brisalchert@gmail.com>
2025-05-13 13:23:02 -0400

    Create QueryService

---
+++ services/QueryService.py
@@ -0,0 +1,32 @@
+from pandas import DataFrame
+from model import DataModel
+from services import AbstractService
+
+
+class QueryService(AbstractService):
+    @property
+    def model(self) -> DataModel:
+        return self._model
+
+    @property
+    def query(self) -> str:
+        return self._query
+
+    @property
+    def last_query_result(self) -> DataFrame:
+        return self._last_query_result
+
+    def __init__(self, model: DataModel):
+        self._model = model
+        self._query = None
+        self._last_query_result = None
+
+    def set_query(self, query: str):
+        self._query = query
+
+    def execute_query(self) -> DataFrame:
+        # TODO: Implement execute_query
+        pass
+
+    def get_last_result(self) -> DataFrame:
+        return self._last_query_result
