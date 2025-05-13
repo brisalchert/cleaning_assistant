55f1c213a17614ef6d7d457399d8f594e20f2e03
Brian Salchert <brisalchert@gmail.com>
2025-05-13 14:21:33 -0400

    Add view models to package init file

--- viewmodel/__init__.py
+++ viewmodel/__init__.py
@@ -1,1 +1,6 @@
 from .ViewModel import ViewModel
+
+from .MainViewModel import MainViewModel
+from .DataViewerViewModel import DataViewerViewModel
+from .AutoCleanViewModel import AutoCleanViewModel
+from .AnalyticsViewModel import AnalyticsViewModel
