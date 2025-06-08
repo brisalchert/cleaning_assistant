6d7e10fec295c4fe75132a3dcab572cf24a5a0cb
Brian Salchert <brisalchert@gmail.com>
2025-06-08 09:57:40 -0400

    Remove unused methods from data model

--- model/data_model.py
+++ model/data_model.py
@@ -25,30 +25,6 @@ class DataModel(QObject):
         self._database[table_name] = table
         self.data_changed.emit(self._database)
 
-    def create_row(self, table_name: str, columns: dict) -> bool:
-        # Check for the table in the database
-        if table_name not in self._database:
-            return False
-
-        # Try to add the row, catching errors for invalid column names
-        try:
-            self._database[table_name] = pd.concat([self._database[table_name], pd.Series(columns)], ignore_index=True)
-        except ValueError as e:
-            print(f"Error adding row to table {table_name}: {e}")
-            return False
-
-        self.data_changed.emit(self._database)
-
-        return True
-
-    def read_row(self, table_name: str, primary_key: str) -> Series:
-        # Check for the table in the database
-        if table_name not in self._database:
-            return pd.Series()
-
-        # Read the row from the database, or return an empty Series if not found
-        return self._database[table_name].get(primary_key, pd.Series())
-
     def update_row(self, table_name: str, new_row_df: DataFrame) -> bool:
         # Check for the table in the database
         if table_name not in self._database:
@@ -61,18 +37,3 @@ class DataModel(QObject):
             self._database[table_name].update(new_row_df)
             self.data_changed.emit(self._database)
             return True
-
-    def delete_row(self, table_name: str, primary_key: str) -> Series:
-        # Check for the table in the database
-        if table_name not in self._database:
-            return pd.Series()
-
-        # Delete the row from the database, or return an empty Series if not found
-        removed_row = self._database[table_name].get(primary_key, pd.Series())
-
-        if not removed_row.empty:
-            self._database[table_name].drop(primary_key, inplace=True)
-
-        self.data_changed.emit(self._database)
-
-        return removed_row
